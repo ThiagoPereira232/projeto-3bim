9b48a192e1e8d8ad8b07c546f1e58a5a20e2eb89
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-07-25 20:42:11 -0300

    player anda e corre

---
+++ rpg/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+
+    private bool _isRunning;
+    private float initialSpeed;
+
+    private Rigidbody2D rig;
+
+    private Vector2 _direction;
+
+    public Vector2 Direction { get => _direction; set => _direction = value; }
+    public bool IsRunning { get => _isRunning; set => _isRunning = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnInput();
+        OnRun();
+    }
+
+    private void FixedUpdate()
+    {
+        OnMove();
+    }
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+}
