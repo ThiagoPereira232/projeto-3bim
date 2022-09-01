6060c92b69ed4f4e9891728bad4286308a3851f6
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-09-01 15:56:56 -0300

    main menu and pause menu

---
+++ rpg2/Assets/Script/Menu/MainMenu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject tutorial;
+    public GameObject credits;
+    public GameObject mainMenu;
+    public Dropdown dropdown;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Village");
+    }
+
+    public void LoadTutorial()
+    {
+        Time.timeScale = 1f;
+        tutorial.SetActive(true);
+        credits.SetActive(false);
+        mainMenu.SetActive(false);
+    }
+
+    public void LoadCredits()
+    {
+        Time.timeScale = 1f;
+        tutorial.SetActive(false);
+        credits.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void BackMenu()
+    {
+        Time.timeScale = 1f;
+        tutorial.SetActive(false);
+        credits.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
